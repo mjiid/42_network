f917327781d2187dfeed6af797dce4b9dd403d5d
mjiid <Habush1610@gmail.com>
2024-01-06 02:26:58 +0100

    Day 03 Done

--- day3/ex08/ft_print_combn.c
+++ day3/ex08/ft_print_combn.c
@@ -1,11 +1,62 @@
 #include <unistd.h>
 
 
-void ft_print_combn(int n) {
-	
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
+void	print_array(char v[], int size, int last)
+{
+	int		i;
+
+	i = 0;
+	if (v[0] == last)
+	{
+		while (i < size)
+			ft_putchar(v[i++]);
+		write(1, ".\n", 2);
+		return ;
+	}
+	while (i < size)
+		ft_putchar(v[i++]);
+	write(1, ", ", 2);
+}
+
+void	ft_print_combn(int n)
+{
+	char	v[10];
+	char	v_max[10];
+	int		flag;
+	int		base;
+
+	if (n <= 0 || n > 10)
+	{
+		write(1, "Insert an 0<n<10\n", 17);
+		return ;
+	}
+
+	for (int i=0; i<n; ++i)
+	{
+		v[i] = i + 48;
+		v_max[i] = (10 - n) + i + 48;
+	}
+
+	print_array(v, n, v_max[0]);
+
+	while (v[0] != v_max[0])
+	{
+		flag = n-1;
+		while (v[flag] == v_max[flag])
+			--flag;
+		base = ++(v[flag]);
+		while (flag < n-1)
+			v[++flag] = ++base;
+		print_array(v, n, v_max[0]);
+	}
+}
 
 int main() {
 	ft_print_combn(4);
+	return 0;
 }
